2b7c59e52bc699cf06245cd0d2c052636b0d9e57
Андрей Кудряшов <andreyka001@gmail.com>
2019-09-16 07:41:38 +0300

    Добавьте файлы проекта.

---
+++ FileRenamer/MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace FileRenamer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Renamer renamer;
+        //private Paragraph paragraph;
+
+        public MainWindow()
+        {
+            renamer = new Renamer();
+            InitializeComponent();
+            ComboMode.SelectedIndex = 0;
+        }
+        
+        // выбор определенных файлов
+        private void BrowseFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Все файлы (*.*)|*.*" + "|Документы PDF (*.PDF)|*.PDF"; 
+            fileDialog.CheckFileExists = true;
+            fileDialog.Multiselect = true;
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                renamer.OpenFileList(fileDialog);
+
+                UpdateOriginalLabels();
+                UpdateNewFileLabels();
+            }
+        }
+        
+        // выбор папки со всеми подпапками и файлами внутри
+        private void BrowseDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog;
+            using (dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.ShowNewFolderButton = false;
+                dialog.Description = "Выберите корневую папку с файлами для переименования";
+                dialog.SelectedPath = System.Environment.SpecialFolder.MyDocuments.ToString();
+                //dialog.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    renamer.OpenDirectory(dialog.SelectedPath);                    
+                }
+                UpdateOriginalLabels();
+                UpdateNewFileLabels();
+                renamer.UpdateNewFileNames();
+            }           
+        }
+        
+        // обновим окошко с оригинальными именами файлов
+        private void UpdateOriginalLabels()
+        {
+            DirPatch.Text = renamer.rootPatch;
+            OriginalList.Text = renamer.rootPatch + "> \n";
+
+            string currentPatch = renamer.rootPatch;
+            for (int i = 0; i < renamer.numFiles; i++)
+            {
+                // если дошли до следующего каталога, выводим его путь
+                if(renamer.fn[i].patch != currentPatch)
+                {
+                    currentPatch = renamer.fn[i].patch;
+                    OriginalList.Text += currentPatch + "> \n";                    
+                }
+                OriginalList.Text += renamer.fn[i].OriginalFileName + "\n";
+            }
+            RenameTemplateBox.Text = renamer.template;
+            ComboMode.SelectedItem = ComboItem_UpDir; //костыль
+        }
+        
+        // обновим окошко с результатами переименования
+        private void UpdateNewFileLabels()
+        {
+            if (RenameList == null)
+                return;
+            // новая версия
+            string currentPatch = renamer.rootPatch;
+            RenameList.Text = currentPatch + "> \n";
+            renamer.UpdateNewFileNames();//////
+            for (int i = 0; i < renamer.numFiles; i++)
+            {
+                if (renamer.fn[i].patch != currentPatch)
+                {
+                    currentPatch = renamer.fn[i].patch;
+                    RenameList.Text += currentPatch + "> \n";
+                    /*paragraph = new Paragraph();
+                    paragraph.Inlines.Add(  new Bold() );*/
+                }
+                RenameList.Text += renamer.fn[i].NewFileName + "\n"; //обновим текст в окошке
+            }
+        }
+
+        private void RenameButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateNewFileLabels();
+            renamer.Rename();
+        }
+        //обработка события изменения текста
+        private void RenameTemplate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            renamer.SetTemplate(RenameTemplateBox.Text); 
+            ComboMode.SelectedItem = Template;
+            UpdateNewFileLabels();            
+        }
+        //обработка события введения текста (не работает!!)
+        private void RenameTemplate_TextInpuit(object sender, System.Windows.Input.TextCompositionEventArgs e)//не работает!?
+        {
+            renamer.SetTemplate(RenameTemplateBox.Text); 
+            ComboMode.SelectedItem = Template;
+            renamer.UpdateNewFileNames();
+            UpdateNewFileLabels();            
+        }
+
+        private void ComboMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem selectedItem = (ComboBoxItem)ComboMode.SelectedItem;
+
+            if (selectedItem == ComboItem_UpDir) 
+            {
+                //renamer.UpdateNewFileNames();
+                RenameTemplateBox.Text = "[имя_каталога]-[имя_файла]";
+                ComboMode.SelectedItem = ComboItem_UpDir; // нужно вернуть, т.к. RenameTemplate_TextChanged сбивает
+                renamer.SetTemplate("[имя_каталога]-[имя_файла]");
+                UpdateNewFileLabels();
+                //MessageBox.Show(selectedItem.Content.ToString() + " ляляля");
+            }
+            if (selectedItem == ComboItem_RootDir) 
+            {
+                //renamer.UpdateNewFileNames();
+                RenameTemplateBox.Text = "[корневой_каталог]-[имя_файла]";
+                ComboMode.SelectedItem = ComboItem_RootDir; // нужно вернуть, т.к. RenameTemplate_TextChanged сбивает
+                renamer.SetTemplate("[корневой_каталог]-[имя_файла]");
+                UpdateNewFileLabels();
+            }
+            if (selectedItem == Template)
+            {
+                //renamer.UpdateNewFileNames();
+                UpdateNewFileLabels();
+            }
+            if (selectedItem == Cancel)
+            {
+                //renamer.UpdateNewFileNames();
+                RenameTemplateBox.Text = "[имя_файла]";
+                ComboMode.SelectedItem = Cancel; // нужно вернуть, т.к. RenameTemplate_TextChanged сбивает
+                renamer.SetTemplate("[имя_файла]");
+                UpdateNewFileLabels();
+            }
+        }
+        private void ReplaceInNameSource_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            renamer.replace_source = ReplaceInNameSource.Text;
+            //renamer.UpdateNewFileNames();
+            UpdateNewFileLabels();
+        }
+
+        private void ReplaceInNameTemplate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            renamer.replace_template = ReplaceInNameTemplate.Text;
+            //renamer.UpdateNewFileNames();
+            UpdateNewFileLabels();
+        }
+
+        private void MenuAboutItem_Click(object sender, RoutedEventArgs e)
+        {
+            AboutBox a = new AboutBox();
+            a.Show();
+        }
+
+        private void MenuExit_Click(object sender, RoutedEventArgs e)
+        {            
+            Close();
+        }
+
+        private void Window_Initialized(object sender, System.EventArgs e)
+        {
+            renamer.Init(this);
+        }
+    }
+}
---
+++ FileRenamer/Renamer.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace FileRenamer
+{
+    struct fileNames
+    {
+        public string patch;
+        public string OriginalFileName;
+        public string PrevFileName;
+        public string NewFileName;
+    }
+
+    class Renamer
+    {
+        MainWindow window;
+        public int numFiles = 0;
+        public fileNames[] fn;
+        public string rootPatch = "";
+        public string template = "_";
+        public string replace_source = "";
+        public string replace_template = "";
+
+        // режимы работы переименоваттеля
+        //public enum modes : int { ROOT_NAME = 0, UP_DIR_NAME, TEMPLATE }
+        //private int mode;
+
+        public Renamer()
+        {            
+            //mode = (int)modes.UP_DIR_NAME;
+        }
+        public void Init(MainWindow win)
+        {
+            window = win;
+        }
+
+        // открываем фалы списком
+        public void OpenFileList(OpenFileDialog fileDialog)
+        {
+            numFiles = fileDialog.FileNames.Count(); //кол-во файлов    
+            fn = new fileNames[numFiles];
+
+            // обрежем имя файла, чтобы получить путь к папке
+            int ind = fileDialog.FileName.LastIndexOf('\\');
+            rootPatch = fileDialog.FileName.Remove(ind + 1); //путь к папке
+            
+            //foreach (string file in SelectedFilesNames)
+            for (int i = 0; i < numFiles; i++)
+            {
+                // здесь наоборот - обрежем путь, чтобы получить имя файла
+                ind = fileDialog.FileNames[i].LastIndexOf('\\'); // отсекаем ненужное                    
+                string str = fileDialog.FileNames[i].Remove(0, ind + 1);
+
+                fn[i].patch = rootPatch;
+                fn[i].PrevFileName = str;
+                fn[i].NewFileName = str;
+                fn[i].OriginalFileName = str;   //Сохраним оригинальное имя, пригодится для отмены
+            }
+        }
+        // открываем каталог со всеми вложениями
+        public void OpenDirectory(string dir)
+        {
+            rootPatch = dir;
+            string currentPatch = rootPatch;
+            string[] files;            
+
+            string extensionPattern = "*.*";
+            try
+            {
+                files = Directory.GetFiles(rootPatch, extensionPattern, SearchOption.AllDirectories);
+                numFiles = files.Count();
+            }
+            catch (UnauthorizedAccessException uex)
+            {
+                files = new string[0];
+                //обработка искл.
+            }
+            catch (Exception ex)
+            {
+                files = new string[0];                
+                //обработка искл.
+            }
+            
+            fn = new fileNames[numFiles];
+            
+            for (int i = 0; i < numFiles; i++)
+            {
+                // обрежем имя файла, чтобы получить путь к папке
+                int ind = files[i].LastIndexOf('\\');
+                currentPatch = files[i].Remove(ind + 1); //путь к папке
+
+                // здесь наоборот - обрежем путь, чтобы получить имя файла
+                ind = files[i].LastIndexOf('\\'); // отсекаем ненужное                    
+                string str = files[i].Remove(0, ind + 1);
+
+                fn[i].patch = currentPatch;
+                fn[i].PrevFileName = str;
+                fn[i].NewFileName = str;
+                fn[i].OriginalFileName = str;   //Сохраним оригинальное имя                
+            }
+        }
+        /// <summary>
+        /// ///////////
+        /// </summary>
+        public void UpdateNewFileNames()
+        {
+            string currentPatch = rootPatch;
+            for (int i = 0; i < numFiles; i++)
+            {                
+                fn[i].NewFileName = ParceString(template, fn[i]); // переименование по шаблону
+                fn[i].NewFileName = ReplaceString(fn[i]); //заменим символы внутри имени файла
+            }
+            if(window !=null)
+                window.StatusBarText.Text = "Задайте шаблон для переименования файлов";
+        }
+
+        //обрезка от имени папки всего предыдущего пути
+        private string ClipPathName(string path)
+        {
+            int ind = path.LastIndexOf('\\');
+            if (ind < 0)
+                return path;
+            path = path.Remove(ind); //путь к папке
+
+            ind = path.LastIndexOf('\\'); // отсекаем ненужное                    
+            if (ind < 0)
+                return path;
+            path = path.Remove(0, ind + 1);
+
+            return path;
+        }
+        //парсим строку шаблона
+        private string ParceString(string str, fileNames fn)
+        {
+            if (str.Contains("[имя_каталога]"))
+            {
+                str = str.Replace("[имя_каталога]", ClipPathName(fn.patch));
+            }
+            if (str.Contains("[корневой_каталог]"))
+            {
+                str = str.Replace("[корневой_каталог]", ClipPathName(rootPatch));
+            }
+            if (str.Contains("[имя_файла]"))
+            {
+                str = str.Replace("[имя_файла]", fn.OriginalFileName);
+            }
+            return str;
+        }
+        private string ReplaceString(fileNames fn)
+        {
+            string str = "";
+
+            /*if (fn.NewFileName == null)
+                str = fn.OriginalFileName;
+            else str = fn.NewFileName;*/
+            str = fn.NewFileName;
+
+            //Замена части имени файла по шаблону
+            if (replace_source != "")
+                str = str.Replace(replace_source, replace_template);
+            return str;
+        }
+
+        public void SetTemplate(string str)
+        {
+            template = str;
+        }
+        public void ReplaceInFileName()
+        {
+
+        }
+        public void Rename()
+        {
+            for (int i = 0; i < numFiles; i++)
+            {
+                window.StatusBarText.Text = "> Переименование файлов: " + i + "из " + numFiles;
+                File.Move(fn[i].patch + fn[i].PrevFileName, fn[i].patch + fn[i].NewFileName);
+                fn[i].PrevFileName = fn[i].NewFileName;
+            }
+            window.StatusBarText.Text = "Переименование файлов Завершено";
+        }
+    }
+}
